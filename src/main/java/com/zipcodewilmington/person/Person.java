53e9da3d73e1f810e3675dfa4b1349b2db45732b
AngelicaSantos <angelica.santos2113@gmail.com>
2019-06-05 18:47:35 -0400

    complete

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -7,29 +7,48 @@ public class Person {
     private String name;
     private int age;
 
-    public Person() {
+    public Person () {
+        this.age=Integer.MAX_VALUE;
+        this.name="";
+    }
+
+
+    public Person(String name, Integer age) {
+        this.name=name;
+        this.age=age;
     }
 
     public Person(int age) {
+        this.age=age;
+        this.name="";
+
     }
 
     public Person(String name) {
+        this.name=name;
+        this.age=Integer.MAX_VALUE;
     }
 
     public Person(String name, int age) {
+        this.name=name;
+        this.age=age;
     }
 
     public void setName(String name) {
+        this.name=name;
     }
 
     public void setAge(int age) {
+        this.age=age;
     }
 
     public String getName() {
-        return null;
+
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+
+        return age;
     }
 }
